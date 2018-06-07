e4d0c8c15bb5c2b2cae227c2bd0b81c050dd27dd
RodrigoSilva93 <rodrigo.santos.silva@outlook.com>
2018-06-06 23:14:10 -0300

    tabelas do banco refeitas, testar os bancos dial e pieces

--- hcbuilder.py
+++ hcbuilder.py
@@ -1,8 +1,8 @@
 from app import app, db
-from app.models import User, Collection, Units, Dial_Move, Dial_Atack, Dial_Defense, Dial_Damage
+from app.models import User, Collection, Dial_Movement, Dial_Attack, Dial_Defense, Dial_Damage, Pieces
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Collection': Collection, 'Units': Units,
-            'Dial_Move': Dial_Move, 'Dial_Atack': Dial_Atack, 'Dial_Defense': Dial_Defense,
-            'Dial_damage': Dial_damage}
+    return {'db': db, 'User': User, 'Collection': Collection, 'Dial_Movement': Dial_Movement,
+            'Dial_Attack': Dial_Attack, 'Dial_Defense': Dial_Defense, 'Dial_Damage': Dial_Damage,
+            'Pieces': Pieces}
